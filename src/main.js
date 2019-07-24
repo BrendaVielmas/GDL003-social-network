73791711b35c1b105ce0739a551bdb3d473c0eeb
Brenda Vielmas <breen.vielmas@gmail.com>
2019-07-24 18:44:35 -0500

    cambios estructurales

--- src/main.js
+++ src/main.js
@@ -6,11 +6,38 @@ document.getElementById("userInformationInTimeline").style.display = "none";
 let username = document.getElementById("enterUsername");
 let name = document.getElementById("enterName");
 
-let signIn = () => {
-	document.getElementById("signOut").style.display = "block"; 
-	document.getElementById("userInformationInTimeline").style.display = "block";
+let createAcountFunction = () => {
+	document.getElementById("logInPage").style.display = "block";
+	document.getElementById("createAcount").style.display = "none";
+	document.getElementById("buttonSignIn").style.display = "none";
+	document.getElementById("textForCreateAcount").style.display = "none";
+	document.getElementById("signInPage").style.display = "block";
+}
+
+let createUser = () => {
+	name = name.value
+	username = username.value
+	let email = document.getElementById("enterEmail").value;
+	let password = document.getElementById("enterPassword").value;
+	let saveEmailAndPassword = window.data.createUser(email, password);
+	document.getElementById("logInPage").style.display = "none";
+	document.getElementById("signInPage").style.display = "none";
+	[].forEach.call(document.querySelectorAll(".autenticationSec"), function(element){
+    element.style.display = "none";
+    });
+	let saveNameAndUsername = window.data.createDataOfUsers(name, username);
+	document.getElementById("userInformationInTimeline").style.display = "block"; 
+	console.log ("I am going to save " + username + name + " to Firestore");
 	document.getElementById("boxForNameInformation").innerHTML = name;
 	document.getElementById("boxForUsernameInformation").innerHTML = username;
+}
+
+let signIn = () => {
+	
+	document.getElementById("signOut").style.display = "block"; 
+	document.getElementById("userInformationInTimeline").style.display = "block";
+	document.getElementById("boxForNameInformation").innerHTML = name.value;
+	document.getElementById("boxForUsernameInformation").innerHTML = username.value;
 	let email = document.getElementById("enterEmail").value;
 	let password = document.getElementById("enterPassword").value;
 	let saveEmailAndPassword = window.data.signIn(email, password);
@@ -27,39 +54,8 @@ let signIn = () => {
 	});
 }
 
-let createAcountFunction = () => {
-	document.getElementById("logInPage").style.display = "block";
-	document.getElementById("createAcount").style.display = "none";
-	document.getElementById("buttonSignIn").style.display = "none";
-	document.getElementById("textForCreateAcount").style.display = "none";
-	document.getElementById("signInPage").style.display = "block";
-}
 
-let createUser = () => {
-	name = name.value
-	username = username.value
-	let email = document.getElementById("enterEmail").value;
-	let password = document.getElementById("enterPassword").value;
-	let saveEmailAndPassword = window.data.createUser(email, password);
-	document.getElementById("enterEmail").style.display = "none";
-	document.getElementById("enterPassword").style.display = "none";
-	document.getElementById("enterUsername").style.display = "none";
-	document.getElementById("enterName").style.display = "none";
-	document.getElementById("buttonRegister").style.display = "none";
-	document.getElementById("createAcount").style.display = "none";
-	[].forEach.call(document.querySelectorAll(".autenticationSec"), function(element){
-    element.style.display = "none";
-    });
-	[].forEach.call(document.querySelectorAll(".principalHome"), function(element){
-    element.style.display = "none";
-	});
-	let saveNameAndUsername = window.data.createDataOfUsers(name, username);
-	console.log ("I am going to save " + username + name + " to Firestore");
-	document.getElementById("boxForNameInformation").style.display = "block";
-	document.getElementById("boxForNameInformation").innerHTML = name;
-	document.getElementById("boxForUsernameInformation").style.display = "block";
-	document.getElementById("boxForUsernameInformation").innerHTML = username;
-}
+
 let signOutFunction = () => {
 	firebase.auth().signOut().then(function() {
 		window.location.reload()
