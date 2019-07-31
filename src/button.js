c1f09d8ae241dee977b734673a9a892965ffe503
PaulinaFranco <dadiante@gmail.com>
2019-07-31 18:30:10 -0500

    se agregan líneas para poner fecha y se inicia función likes

--- src/button.js
+++ src/button.js
@@ -1,23 +1,34 @@
 let post = document.getElementById("timelinePost");
 
+
 const db = firebase.firestore()
 db.collection("Users").get().then((querySnapshot) => {
 	querySnapshot.forEach((doc) => {
 			console.log(doc.data());
 			let postOfUser = doc.data();
 			document.getElementById("sectionWithPost").innerHTML += `
-			<section  class = "postInBox"> 
-			<p>Fecha: ${postOfUser.date}</p>
+			<section  class = "postInBox">
+			<p>Fecha: ${postOfUser.dates}</p>
 			<p>Estado: ${postOfUser.message}</p>
-			<button class="button" id="buttonForEditpost">Editar</button>
-			<button class="button" id="buttonForDeletePost">Eliminar</button>
+			<section id="buttonForLike">
+				<button > &#x1F49B;  </button>
+				<p>Me gusta ${postOfUser.likes}</p>
+			</section>
+				<button class="button" id="buttonForEditpost">Editar</button>
+				<button class="button" id="buttonForDeletePost">Eliminar</button>
 			</section>`;
+
 	});
 });
 
+/*document.getElementById("buttonForLike").addEventListener('click', () => {
+		alert('asdasd');
+	});*/
+
+
 const signOutButton= () => {
-	console.log("in: button.js signOutButton")
-	window.data.signOutFunction()
+	console.log("in: button.js signOutButton");
+	window.data.signOutFunction();
 	location.assign("index.html");
 };
 
@@ -25,9 +36,17 @@ let createPostFunction = (docRef) => {
 	let status = document.querySelector("[type=radio]:checked").value;
 	console.log("in: button.js createPostFunction");
 	let message = post.value;
-	window.data.createPost(message, status);
-	
+
+	let day = new Date().toLocaleDateString();
+	let hour = new Date().toLocaleTimeString();
+
+	let dates = " ";
+		dates = day + " " + hour;
+		console.log(dates);
+
+	window.data.createPost(message, status, dates);
 };
+
 
 document.getElementById("signOut").addEventListener("click", signOutButton);
 document.getElementById("buttonForCreatePost").addEventListener("click", createPostFunction);
